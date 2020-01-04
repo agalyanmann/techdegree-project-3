ab1231759d3189a0e978f736b0e7446d2e5d95fb
Andrew Galyan-Mann <andrew.galyanmann@icloud.com>
2020-01-04 16:13:54 -0500

    T-Shirt Menu
    Shirt animation complete.
    
    In progress of displaying correct colors to selected theme.

--- js/script.js
+++ js/script.js
@@ -1,22 +1,36 @@
 //set focus to first input box on page
-$('#name').focus();
+$("#name").focus();
 //hide other input field
-$('#other-title').hide();
+$("#other-title").hide();
 //if other is selected for title
-$('#title').change(function(){
-    const inputValue = $(this).val()
-    //show other input field
-    if (inputValue === 'other') {
-        $('#other-title').fadeIn(1000);
-    } else {
-        $('#other-title').fadeOut(1000);
-    }
+$("#title").on("change", function() {
+  const $inputValue = $(this).val();
+  //show other input field
+  if ($inputValue === "other") {
+    $("#other-title").fadeIn(1000);
+  } else {
+    $("#other-title").fadeOut(1000);
+  }
 });
 //hide color select menu
-$('#color').hide();
+$("#color").hide();
 //place text instructing to select theme
-$('#colors-js-puns').append('<h3 class="theme-instuctions">Please select a shirt theme.</h3>');
-//if theme1
-    //show relevant colors
-//if theme2
-    //show relevant colors
+const $themeInsturctions =
+  '<h4 id="theme-instructions">Please select a T-shirt theme.</h4>';
+$("#colors-js-puns").append($themeInsturctions);
+//color menu transition
+$("#design").on("change", function() {
+  const $userSelection = $(this).val();
+  if ($userSelection === "js puns" || $userSelection === "heart js") {
+    $("#theme-instructions").fadeOut();
+    $("#color")
+      .delay(700)
+      .fadeIn();
+  } else {
+    $("#color").fadeOut();
+    $("#theme-instructions")
+      .delay(700)
+      .fadeIn();
+  }
+});
+
