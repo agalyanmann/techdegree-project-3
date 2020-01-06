5ad7bae2a05b348c2343649acc5f5b51083c902d
Andrew Galyan-Mann <andrew.galyanmann@icloud.com>
2020-01-05 20:07:22 -0500

    Activities Price Calculator
    User now will see total price calculated based on what events the select or deslect in the form.

--- js/script.js
+++ js/script.js
@@ -1,25 +1,27 @@
 /*---------------------------------------------------
-Initial page focus and toggle input for "other" option
+Initial page focus and toggle input for 'other' option
 ---------------------------------------------------*/
+
 $("#name").focus();
 
-//hide "other" input field
+//hide 'other' input field
 $("#other-title").hide();
 
-//"other" input field toggle
+//'other' input field toggle
 $("#title").on("change", function() {
   const $inputValue = $(this).val();
   //show other input field
   if ($inputValue === "other") {
-    $("#other-title").fadeIn(1000);
+    $("#other-title").slideDown();
   } else {
-    $("#other-title").fadeOut(1000);
+    $("#other-title").slideUp();
   }
 });
 
 /*---------------------------------------------------
 Color option animation and validation block
 ---------------------------------------------------*/
+
 $("#color").hide();
 
 //instruction text
@@ -44,11 +46,45 @@ $("#design").on("change", function() {
 
   //validate colors to proper theme
   if ($userSelection === "heart js") {
+    $('#color option[value="tomato"]').attr("selected", "");
+    $('#color option[value="cornflowerblue"]').attr("selected", false);
     $("#color option").hide();
-    $("#color option:not(:contains('Pun'))").show();
+    $('#color option:not(:contains("Pun"))').show();
   }
   if ($userSelection === "js puns") {
+    $('#color option[value="cornflowerblue"]').attr("selected", "");
+    $('#color option[value="tomato"]').attr("selected", false);
     $("#color option").hide();
-    $("#color option:contains('Pun')").show();
+    $('#color option:contains("Pun")').show();
+  }
+});
+
+/*---------------------------------------------------
+Activities block
+---------------------------------------------------*/
+
+//new elements, global variables
+const totalElement = '<p id="total"><p>';
+$('.activities').append(totalElement);
+$('#total').hide();
+let activitiesTotal = 0;
+
+//input listener with calculator of total price
+$('.activities').on('change', 'input', function () {
+  const $inputRef = $(this).is(':checked');
+  console.log($inputRef);
+  const $activityCost = parseInt($(this).attr('data-cost'));
+  console.log($activityCost);
+
+  if ($inputRef){
+    activitiesTotal += $activityCost;
+    if (activitiesTotal > 0) {
+      $('#total').text('Total: $' + activitiesTotal).slideDown();
+    }
+  } else {
+    activitiesTotal -= $activityCost;
+    if (activitiesTotal === 0) {
+    $('#total').text('Total: $' + activitiesTotal).slideUp();
+    }
   }
 });
